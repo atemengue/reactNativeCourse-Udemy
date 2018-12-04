b8bda71d08beb39b546d7a10a8ada0953da4b172
atemengue <moafembe@gmail.com>
2018-12-04 05:39:18 +0100

    understand redux-thunk and async request

--- src/components/actions/index.js
+++ src/components/actions/index.js
@@ -1,6 +1,32 @@
-export const fetchPosts = () => {
-  return {
-    type: 'FETCH_POSTS'
-  };
+import jsonPlaceHolder from "../apis/jsonPlaceHolder";
+
+//refactoring//
+export const fetchPosts =  () => async dispatch => {
+    const response = await  jsonPlaceHolder.get('/posts');
+  
+    dispatch({ type: 'FETCH_POSTS', payload: response })
 }
 
+export const selectPost = () => {
+  return {
+    type: 'SELECT_POST'
+  }
+}
+
+
+// export const fetchPosts =  () => {
+//   return async function(dispatch, getState) {
+//     const response = await  jsonPlaceHolder.get('/posts');
+    
+//     dispatch({
+//       type: 'FETCH_POSTS', payload: response
+//     })
+// }
+
+// }
+
+// export const selectPost = () => {
+//   return {
+//     type: 'SELECT_POST'
+//   }
+// }
