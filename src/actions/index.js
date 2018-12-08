06fcef54d2b761e09d1ec63f82d8ecdacd0fac9d
atemengue <moafembe@gmail.com>
2018-12-08 04:39:37 +0100

    Dispatching Correct Values

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,12 @@
-import jsonPlaceHolder from "../apis/jsonPlaceHolder";
+
+import jsonPlaceHolder from '../apis/jsonPlaceHolder';
 
 //refactoring//
 export const fetchPosts =  () => async dispatch => {
     const response = await  jsonPlaceHolder.get('/posts');
   
-    dispatch({ type: 'FETCH_POSTS', payload: response })
+    dispatch({ type: 'FETCH_POSTS', payload: response.data })
 }
-
-export const selectPost = () => {
-  return {
-    type: 'SELECT_POST'
-  }
-}
-
 
 // export const fetchPosts =  () => {
 //   return async function(dispatch, getState) {
