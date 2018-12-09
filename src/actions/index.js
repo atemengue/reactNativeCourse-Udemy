5a403b49d3a98f64a0ec9746ffb6e8e6c7198575
atemengue <moafembe@gmail.com>
2018-12-09 04:42:15 +0100

    App wrapup and Review

--- src/actions/index.js
+++ src/actions/index.js
@@ -55,7 +55,17 @@ export const fetchUser = (id) => async dispatch => {
 
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
     await dispatch(fetchPosts());
-    const userIds = _.uniq(_.map(getState().posts, 'userId'));
-    console.log(userIds)
-    userIds.forEach(id => dispatch(fetchUser(id)));
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value()
 };  
+
+
+// export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+//     await dispatch(fetchPosts());
+//     const userIds = _.uniq(_.map(getState().posts, 'userId'));
+//     console.log(userIds)
+//     userIds.forEach(id => dispatch(fetchUser(id)));
+// };  
